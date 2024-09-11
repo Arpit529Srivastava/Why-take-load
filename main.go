4644a9081b404a7749d11e364e81eddffdd019c1
arpit529srivastava <arpitsrivastava529@gmail.com>
2024-09-12 04:00:45 +0530

    removed unnecessary comments

--- main.go
+++ main.go
@@ -10,38 +10,38 @@ import (
 
 // Server interface defines methods for a load balancer to interact with backend servers
 type Server interface {
-	Address() string            // Method to get the server address
-	IsAlive() bool              // Method to check if the server is alive
-	Serve(rw http.ResponseWriter, r *http.Request) // Method to serve the HTTP request
+	Address() string            
+	IsAlive() bool              
+	Serve(rw http.ResponseWriter, r *http.Request) 
 }
 // simpleserver struct represents a simple backend server with an address and reverse proxy
 type simpleserver struct {
-	addr  string                 // Server address
-	proxy httputil.ReverseProxy   // Reverse proxy to forward the requests
+	addr  string                 
+	proxy httputil.ReverseProxy   
 }
 
 // Constructor function for creating a new simpleserver instance
 func newsimplesever(addr string) *simpleserver {
-	serverurl, err := url.Parse(addr) // Parse the server address as a URL
-	handlErr(err)                     // Handle any errors that occur during parsing
+	serverurl, err := url.Parse(addr) 
+	handlErr(err)                     
 	return &simpleserver{
-		addr:  addr,                                // Set the address
-		proxy: *httputil.NewSingleHostReverseProxy(serverurl), // Create a new reverse proxy to the given server
+		addr:  addr,                                
+		proxy: *httputil.NewSingleHostReverseProxy(serverurl),
 	}
 }
 
 // LoadBalancer struct contains the logic for distributing requests to servers
 type LoadBalancer struct {
-	port       string   // Port where the load balancer is running
-	roundrobin int      // Index to keep track of which server to use next
-	servers    []Server // Slice of servers that the load balancer will distribute traffic to
+	port       string   
+	roundrobin int      
+	servers    []Server 
 }
 // NewLoadBalancer constructor initializes a LoadBalancer with a given port and list of servers
 func NewLoadBalancer(port string, servers []Server) *LoadBalancer {
 	return &LoadBalancer{
 		port:       port,
-		roundrobin: 0,      // Start with the first server in the list
-		servers:    servers, // Set the list of backend servers
+		roundrobin: 0,      
+		servers:    servers, 
 	}
 }
 func (s *simpleserver) Address() string { 
@@ -52,23 +52,23 @@ func (s *simpleserver) IsAlive() bool {
 }
 // Serve method forwards the HTTP request to the backend server using the reverse proxy
 func (s *simpleserver) Serve(rw http.ResponseWriter, r *http.Request) {
-	s.proxy.ServeHTTP(rw, r) // Use the reverse proxy to handle the request
+	s.proxy.ServeHTTP(rw, r) 
 }
 // getNextAvailableServer method implements round-robin selection of servers
 func (lb *LoadBalancer) getNextAvailableServer() Server {
-	server := lb.servers[lb.roundrobin%len(lb.servers)] // Select the next server in the list
-	for !server.IsAlive() {  // Check if the server is alive; if not, move to the next one
+	server := lb.servers[lb.roundrobin%len(lb.servers)] 
+	for !server.IsAlive() {  
 		lb.roundrobin++
 		server = lb.servers[lb.roundrobin%len(lb.servers)]
 	}
-	lb.roundrobin++ // Move to the next server for future requests
-	return server   // Return the available server
+	lb.roundrobin++ 
+	return server   
 }
 // serveProxy method forwards the request to the selected backend server
 func (lb *LoadBalancer) serveProxy(rw http.ResponseWriter, r *http.Request) {
-	targetServer := lb.getNextAvailableServer()  // Get the next available server
-	fmt.Printf("forwarding request to address %q \n", targetServer.Address()) // Log the target server
-	targetServer.Serve(rw, r) // Forward the request to the selected server
+	targetServer := lb.getNextAvailableServer() 
+	fmt.Printf("forwarding request to address %q \n", targetServer.Address()) 
+	targetServer.Serve(rw, r) 
 }
 
 // handlErr function handles any errors, printing the error message and exiting the program
@@ -78,12 +78,12 @@ func handlErr(err error) {
 		os.Exit(1) // Exit if an error occurs
 	}
 }
-// main function is the entry point of the application
+
 func main() {
 	// List of backend servers to forward traffic to
 	servers := []Server{
 		newsimplesever("https://facebook.com"),   // Backend server 1
-		//newsimplesever("https://www.github.com"), // (commented out)
+		newsimplesever("https://www.github.com"), // Backend server 3
 		newsimplesever("https://www.apple.com"),  // Backend server 2
 	}
 	lb := NewLoadBalancer("8000", servers) // Create a new load balancer listening on port 8000
