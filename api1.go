be28e990f143fea0aeb3f186183ff6489b264f7c
arpit529srivastava <arpitsrivastava529@gmail.com>
2024-09-14 16:25:23 +0530

    api for fetching all the movies

--- api1.go
+++ api1.go
@@ -1,9 +1,34 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
-func getBooks(c *gin.Context){
+type Movies struct {
+	ID string `json:"id"`
+	Name string `json:"name"`
+	Release int `json:"date"`
+	Collection string `json:"money"`
+}
+
+var movies = []Movies{
 	
+		{ID: "123", Name:"Intersteller", Release: 2014, Collection: "1.23 Crores"},
+		{ID: "673", Name:"Shutter Island", Release: 2010, Collection: "186.4 Crores"},
+		{ID: "221", Name:"Tenet", Release: 2020, Collection: "20.4 Crores"},
+		{ID: "829", Name:"The Prestige", Release: 2006, Collection: "185 Millions"},
+		{ID: "234", Name:"Memento", Release: 2000, Collection: "9 Millions"},
+		{ID: "454", Name:"Dunkirk", Release: 2017, Collection: "530 Millions"},
+	
+}
+
+func GetMovies(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, movies)
+}
+func main() {
+	router:= gin.Default()
+	router.GET("/", GetMovies)
+	router.Run("localhost:8081")
 }
