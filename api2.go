afb9278a406a9200bee2e8a0b216606f9a66a1b1
arpit529srivastava <arpitsrivastava529@gmail.com>
2024-09-16 16:02:06 +0530

    mongodb intergrated

--- api2.go
+++ api2.go
@@ -1,29 +1,71 @@
 package main
 
 import (
-	//"errors"
+	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"net/http"
+	"time"
 )
 
 type book struct {
-	ID       string `json:"id"`
-	Title    string `json:"title"`
-	Author   string `json:"author"`
-	Quantity int    `json:"quantity"`
+	ID       string `json:"id" bson:"_id, omitempty"`
+	Title    string `json:"title" bson:"title"`
+	Author   string `json:"author" bson:"author"`
+	Quantity int    `json:"quantity" bson:"quantity"`
 }
 
-var books = []book{
-	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
-	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
-	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
+var booksCollection *mongo.Collection
+
+// connecting to mongoDB
+func connectoMongoDb() (*mongo.Client, error) {
+	clientoptions := options.Client().ApplyURI("mongodb://localhost:27017/")
+	client, err := mongo.NewClient(clientoptions)
+	if err != nil {
+		return nil, err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("connected to mongodb")
+	return client, nil
+
 }
 
 func getBooks(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	cursor, err := booksCollection.Find(ctx, bson.M{})
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "cannot retrive the data"})
+		return
+	}
+	var books []book
+	if err = cursor.All(ctx, &books); err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "cannot retrive the data"})
+		return
+	}
 	c.IndentedJSON(http.StatusOK, books)
+
 }
-func main(){
-	router:= gin.Default()
-	router.GET("/",getBooks)
+func main() {
+	client, err := connectoMongoDb()
+	if err != nil {
+		panic(err)
+	}
+	booksCollection = client.Database("Library").Collection("books")
+	router := gin.Default()
+	router.GET("/", getBooks)
 	router.Run("localhost:8082")
 }
--- api3.go
+++ api3.go
@@ -1,36 +1,72 @@
 package main
 
 import (
-	"net/http"
-
+	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"net/http"
+	"time"
 )
 
-
 type Student struct {
-	ID      string `json:"name"`
-	Name    string `json:"s_name"`
-	Age     int    `json:"age"`
-	Class   string `json:"class"`
-	Section string `json:"section"`
+	ID      string `json:"name" bson:"_id, omitempty"`
+	Name    string `json:"s_name" bson:"Name"`
+	Age     int    `json:"age" bson:"Age"`
+	Class   string `json:"class" bson:"Class"`
+	Section string `json:"section" bson:"Section"`
 }
 
-var students = []Student{
-	{ID: "1ds23is023", Name: "Arpit_Srivastava", Age: 20, Class: "3", Section: "A"},
-	{ID: "1ds23is021", Name: "Anurag_Jain", Age: 20, Class: "3", Section: "A"},
-	{ID: "1ds23is022", Name: "Ananaya_Gowda", Age: 20, Class: "3", Section: "A"},
-	{ID: "1ds23is020", Name: "Khushi_Agrawal", Age: 20, Class: "3", Section: "A"},
-	{ID: "1ds23is024", Name: "Harsh_Gupta", Age: 20, Class: "3", Section: "A"},
-	{ID: "1ds23is025", Name: "Aaskar_Rana", Age: 20, Class: "3", Section: "A"},
+var StudentCollection *mongo.Collection
+
+// connecting to mongoDB
+func connectoMongoDb() (*mongo.Client, error) {
+	clientoptions := options.Client().ApplyURI("mongodb://localhost:27017/")
+	client, err := mongo.NewClient(clientoptions)
+	if err != nil {
+		return nil, err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("connected to mongodb")
+	return client, nil
+
 }
 
-func GetAllStudentsNames(c *gin.Context){
-	c.IndentedJSON(http.StatusOK,students)
+func GetAllStudentsNames(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	cursor, err := StudentCollection.Find(ctx, bson.M{})
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "cannot retrive the data"})
+		return
+	}
+	var student []Student
+	if err = cursor.All(ctx, &student); err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "cannot retrive the data"})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, student)
 }
 
-func main(){
-	router:= gin.Default()
-	router.GET("/",GetAllStudentsNames)
+func main() {
+	client, err := connectoMongoDb()
+	if err != nil {
+		panic(err)
+	}
+	StudentCollection = client.Database("school").Collection("students")
+	router := gin.Default()
+	router.GET("/", GetAllStudentsNames)
 	router.Run("localhost:8083")
 }
-
